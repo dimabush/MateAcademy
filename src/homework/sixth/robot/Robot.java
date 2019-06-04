e0ae7da2cef8d0a621029b4e47a98b6a3c0c52e0
dimabush <dim.bush.ua@gmail.com>
2019-06-04 11:55:11 +0300

    delete unnecessary catch block and done some CheckStyle

--- src/homework/sixth/robot/Robot.java
+++ src/homework/sixth/robot/Robot.java
@@ -2,17 +2,14 @@ package homework.sixth.robot;
 
 public class Robot {
   public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-    for(int i = 0; i < 3; i++){
-      try(RobotConnection robotConnection = robotConnectionManager.getConnection()){
+    for (int i = 0; i < 3; i++) {
+      try (RobotConnection robotConnection = robotConnectionManager.getConnection()) {
         robotConnection.moveRobotTo(toX, toY);
         i = 3;
-      } catch (RobotConnectionException e){
-        if(i == 2){
+      } catch (RobotConnectionException e) {
+        if (i == 2) {
           throw new RobotConnectionException("Third try failed", e);
         }
-      } catch (Exception exception){
-        i = 3;
-        throw exception;
       }
     }
   }
