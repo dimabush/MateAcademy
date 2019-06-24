3615bbe16e92c7e89fed071cdd27b494d2d5ebaf
dimabush <dim.bush.ua@gmail.com>
2019-06-24 17:05:22 +0300

    move fields and 'return' in try block

--- src/homework/thirteenth/Deserialization.java
+++ src/homework/thirteenth/Deserialization.java
@@ -6,18 +6,16 @@ import java.io.ObjectInputStream;
 
 public class Deserialization {
   public static Animal[] deserializeAnimalArray(byte[] data) {
-    Animal[] animals;
-    int size;
     ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
     try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
-      size = objectInputStream.readInt();
-      animals = new Animal[size];
+      int size = objectInputStream.readInt();
+      Animal[] animals = new Animal[size];
       for (int i = 0; i < size; i++) {
         animals[i] = (Animal) objectInputStream.readObject();
       }
+      return animals;
     } catch (ClassNotFoundException | IOException | ClassCastException e) {
       throw new IllegalArgumentException(e);
     }
-    return animals;
   }
 }
