4b656f23931e7549c6b2e65c8351a8c49a5d76ae
dimabush <dim.bush.ua@gmail.com>
2019-06-27 08:54:07 +0300

    Update src/homework/fifteenth/bd/Storage.java
    Co-Authored-By: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/homework/fifteenth/bd/Storage.java
+++ src/homework/fifteenth/bd/Storage.java
@@ -7,6 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
-  public static final List<Bet> bets = new ArrayList<>();
+  public static final List<Bet> BETS = new ArrayList<>();
   public static final List<Human> humans = new ArrayList<>();
 }
