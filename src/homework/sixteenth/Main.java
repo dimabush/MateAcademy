99294f28aa083515c88eaa8963a61d79a6e6bf3f
dimabush <dim.bush.ua@gmail.com>
2019-07-01 01:38:26 +0300

    temporary commit

---
+++ src/homework/sixteenth/Main.java
@@ -0,0 +1,19 @@
+package homework.sixteenth;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+
+  public static class MailMessage {
+
+  }
+
+  public static class Salary {
+    // implement here
+  }
+
+  public static class MailService {
+    // implement here
+  }
+}
